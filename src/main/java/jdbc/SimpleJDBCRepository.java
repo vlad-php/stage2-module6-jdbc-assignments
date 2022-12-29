894bc7bf0c7fcfce557ab6717947a9adcb9b6cef
vlad-php <55630588+vlad-php@users.noreply.github.com>
2022-12-29 23:55:34 +0100

    Add files via upload

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -112,7 +112,7 @@ public class SimpleJDBCRepository {
         }
     }
 
-    private void deleteUser(Long userId) {
+    public void deleteUser(Long userId) {
         try {
             Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(deleteUser);
