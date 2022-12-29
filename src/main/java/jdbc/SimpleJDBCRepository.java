b6b5b556d4d875bc65301e7776f663b000f210ff
vlad-php <55630588+vlad-php@users.noreply.github.com>
2022-12-29 23:51:14 +0100

    Add files via upload

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -1,5 +1,5 @@
 package jdbc;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,30 +10,21 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class SimpleJDBCRepository {
+    private final CustomDataSource dataSource = CustomDataSource.getInstance();
 
-    private Connection connection = new CustomConnector().getConnection(CustomDataSource.getInstance().getUrl(), CustomDataSource.getInstance().getName(), CustomDataSource.getInstance().getPassword());
+    private Connection connection = null;
     private PreparedStatement ps = null;
     private Statement st = null;
-    public SimpleJDBCRepository(Connection connection, PreparedStatement ps, Statement st) {
-        CustomConnector customConnector = new CustomConnector();
-        CustomDataSource customDataSource = CustomDataSource.getInstance();
-        this.connection = customConnector.getConnection(customDataSource.getUrl(), customDataSource.getName(), customDataSource.getPassword());
-        this.ps = ps;
-        try {
-            this.st = connection.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     private static final String createUserSQL = """
             INSERT INTO myusers(
             firstname, lastname, age)
             VALUES (?, ?, ?);
             """;
-    private static final String updateUserSQL = """
+    private static final String updateUserSQL ="""
             UPDATE myusers
             SET firstname=?, lastname=?, age=?
             WHERE id = ?
@@ -56,23 +47,25 @@ public class SimpleJDBCRepository {
 
     public Long createUser(User user) {
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(createUserSQL);
-            ps.setString(1, user.getFirstName());
-            ps.setString(2, user.getLastName());
-            ps.setInt(3, user.getAge());
-            ResultSet rs = ps.executeQuery();
-            return rs.next() ? rs.getLong(1) : null;
+            ps.setString(1,user.getFirstName());
+            ps.setString(2,user.getLastName());
+            ps.setInt(3,user.getAge());
+            ps.execute();
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            return generatedKeys.next() ? generatedKeys.getLong(1) : null;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-
     public User findUserById(Long userId) {
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(findUserByIdSQL);
-            ps.setInt(1, Math.toIntExact(userId));
-            ResultSet rs = ps.executeQuery();
-            return rs.next() ? parsUser(rs) : null;
+            ps.setLong(1,userId);
+            ResultSet resultSet = ps.executeQuery();
+            return resultSet.next() ? map(resultSet) : null;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -80,53 +73,57 @@ public class SimpleJDBCRepository {
 
     public User findUserByName(String userName) {
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(findUserByNameSQL);
-            ps.setString(1, userName);
-            ResultSet rs = ps.executeQuery();
-            return rs.next() ? parsUser(rs) : null;
+            ps.setString(1,userName);
+            ResultSet resultSet = ps.executeQuery();
+            return resultSet.next() ? map(resultSet) : null;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public List<User> findAllUser() {
-        List<User> users = new ArrayList<>();
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(findAllUserSQL);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                users.add(parsUser(rs));
+            List<User> users = new ArrayList<>();
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                users.add(map(resultSet));
             }
-        } catch (SQLException e) {
+            return users;
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
-        return users;
     }
 
     public User updateUser(User user) {
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(updateUserSQL);
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
             ps.setInt(3, user.getAge());
             ps.setLong(4, user.getId());
             return ps.executeUpdate() != 0 ? findUserById(user.getId()) : null;
-        } catch (SQLException e) {
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
     }
 
-    public void deleteUser(Long userId) {
+    private void deleteUser(Long userId) {
         try {
+            Connection connection = dataSource.getConnection();
             ps = connection.prepareStatement(deleteUser);
-            ps.setInt(1, Math.toIntExact(userId));
+            ps.setLong(1, userId);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
     }
+    private User map(ResultSet rs) throws SQLException {
 
-    private User parsUser(ResultSet rs) throws SQLException {
         return User.builder()
                 .id(rs.getLong("id"))
                 .firstName(rs.getString("firstname"))
